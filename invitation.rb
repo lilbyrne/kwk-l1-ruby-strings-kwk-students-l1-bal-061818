d55f4c4273365e344f73f5a16f281e83472f5697
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-22 01:24:35 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -18,4 +18,6 @@ def invitation
   puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30."
   puts "Sincerely,"
   puts "#{host_name}"
+end 
+invitation
 end
